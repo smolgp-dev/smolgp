6004769a66d8c9d777e51723713287117b4356c7
Ryan Rubenzahl <rrubenzahl@gmail.com>
2025-10-02 14:46:57 -0400

    placeholders for integrated version of kalman/rts

---
+++ src/ssmolgp/solvers/integrated/__init__.py
@@ -0,0 +1,15 @@
+r"""
+This subpackage implements the Kalman filter and RTS smoother for an augmented
+state space model which includes an integral state as a 
+:class:`IntegratedStateSpaceSolver`. This is intended to be used with
+:class:`ssmolgp.kernels.integrated` state space models to properly account
+for integrated (e.g. exposure-averaged) measurements. 
+
+TODO: add documentation from the paper as a refresher/explainer of the augmented approach
+        see this file in tinygp for example doc style
+        
+"""
+
+__all__ = ["IntegratedStateSpaceSolver"]
+
+from ssmolgp.solvers.integrated.solver import IntegratedStateSpaceSolver
