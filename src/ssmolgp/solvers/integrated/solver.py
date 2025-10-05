4953b52fa3cf1a87b68a69ec742a45430847b911
Ryan Rubenzahl <rrubenzahl@gmail.com>
2025-10-05 18:02:49 -0400

    placeholder class for imports to work

---
+++ src/ssmolgp/solvers/integrated/solver.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+__all__ = ["IntegratedStateSpaceSolver"]
+
+from typing import Any
+
+import jax
+import jax.numpy as jnp
+import equinox as eqx
+
+from tinygp.helpers import JAXArray
+from tinygp.noise import Noise
+from tinygp.solvers.quasisep.solver import QuasisepSolver
+from ssmolgp.kernels.base import StateSpaceModel
+from ssmolgp.solvers.kalman import KalmanFilter
+from ssmolgp.solvers.rts import RTSSmoother
+
+class IntegratedStateSpaceSolver(eqx.Module):
+    """
+    A solver that uses ``jax.lax.scan`` to implement Kalman filtering 
+    and RTS smoothing for integrated measurements
+    """
+
+    X: JAXArray
+    y: JAXArray | None
+    kernel : StateSpaceModel
+    noise  : Noise
+
+    def __init__(
+        self,
+        kernel: StateSpaceModel,
+        X: JAXArray,
+        y: JAXArray | None,
+        noise: Noise,
+    ):
+        """Build a :class:`IntegratedStateSpaceSolver` for a given kernel and coordinates
+
+        Args:
+            kernel: The kernel function.
+            X: The input coordinates.
+            y: The measurement values.
+            noise: The noise model for the process.
+        """
+        self.kernel = kernel
+        self.X = X
+        self.y = y
+        self.noise = noise
