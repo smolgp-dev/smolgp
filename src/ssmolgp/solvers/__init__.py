a57fdedf7cfd9c47e3a6b48b3aba8a08183f83fe
Ryan Rubenzahl <rrubenzahl@gmail.com>
2025-10-09 23:44:54 -0400

    udpate doc

--- src/ssmolgp/solvers/__init__.py
+++ src/ssmolgp/solvers/__init__.py
@@ -1,11 +1,10 @@
 """
 In ``ssmolgp``, "solvers" provide a swappable low-level interface for
 implementing the filtering and smoothing algorithms required to condition 
-Gaussian Process models. At the moment, ``ssmolgp`` includes two solvers,
-but new solvers can be implemented as external packages or as pull requests 
-to the main ``ssmolgp`` GitHub project.
+Gaussian Process models. New solvers can be implemented as external packages 
+or as pull requests to the main ``ssmolgp`` GitHub project.
 
-The two built in solvers are:
+The built in solvers are:
 
 1. :class:`StateSpaceSolver`: A solver that uses the standard Kalman filter
    and RTS smoothing algorithms to derive the optimal predictive means and
@@ -14,13 +13,13 @@ The two built in solvers are:
    implemented in `ssmolgp.kernels.base`. [TODO: compare benchmarking
    to `tinygp.solvers.QuasisepSolver`].
 
-2. :class:`IntegratedStateSpaceSolver`: An experiemental scalable solver that exploits the
-   "quasiseparable" structure found in many GP covariance matrices to make the
-   required linear algabra possible in linear scaling with the size of the
-   dataset. These methods were previously implemented as part of the `celerite
-   <https://celerite.readthedocs.io>`_ project.
+2. :class:`IntegratedStateSpaceSolver`: TODO....
 
-Up to numerical precision, these are both *exact* solvers.
+3. :class:`ParallelStateSpaceSolver`: TODO....
+
+4. :class:`IntegratedParallelStateSpaceSolver`: TODO....
+
+Up to numerical precision, these are all *exact* solvers.
 
 Users generally won't instantiate these solvers directly; ``ssmolgp`` should
 automatically use the appropriate one given the form of the kernel.
