73031d1e2fc92bbd4a22d0fd5da6ff38e4c62087
Ryan Rubenzahl <rrubenzahl@gmail.com>
2025-10-21 14:27:55 -0400

    docs

--- src/ssmolgp/kernels/integrated.py
+++ src/ssmolgp/kernels/integrated.py
@@ -34,6 +34,11 @@ from ssmolgp.kernels import StateSpaceModel
 from ssmolgp.helpers import Q_from_VanLoan
 
 class IntegratedStateSpaceModel(StateSpaceModel):
+    """
+    A generic class that augments a :class:`StateSpaceModel` 
+    obejct (which has state `x`) with an integral state `z`, 
+    to model the joint state `X = [x; z]`.
+    """
 
     ## Same functions as StateSpaceModel
     ## put into augmented form, plus:
@@ -41,6 +46,78 @@ class IntegratedStateSpaceModel(StateSpaceModel):
     ##   - integrated_process_noise
     def __init__(self):
         pass
+
+
+
+    dimension: JAXArray | float = eqx.field(static=True) # dimensionality $d$ of the state vector
+
+    @abstractmethod
+    def design_matrix(self) -> JAXArray:
+        """The design (also called the feedback) matrix for the process, $F$"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def stationary_covariance(self) -> JAXArray:
+        """The stationary covariance of the process, Pinf"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def observation_model(self, X: JAXArray) -> JAXArray:
+        """The observation model for the process, $H$"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def noise(self) -> JAXArray:
+        ''' The spectral density of the white noise process, $Q_c$ '''
+        raise NotImplementedError
+    
+    @abstractmethod
+    def noise_effect_matrix(self) -> JAXArray:
+        ''' The noise effect matrix L, by default this is [0,1]'''
+        raise NotImplementedError
+    
+    def transition_matrix(self, X1: JAXArray, X2: JAXArray) -> JAXArray:
+        """
+        The transition matrix between two states at coordinates X1 and X2, $A_k$
+
+        Default behavior uses jax.scipy.linalg.expm(self.design_matrix() * (X2 - X1)),
+        which is appropriate for stationary kernels defined by a linear Gaussian SSM.
+       
+       Overload this method if you have a more general model or simply wish to
+        define the transition matrix analytically.
+        """
+        F = self.design_matrix()
+        dt = X2 - X1
+        return expm(F * dt)
+ 
+    def process_noise(self, X1: JAXArray, X2: JAXArray) -> JAXArray:
+        """
+        The process noise matrix $Q_k$
+        
+        Default behavior computes Q from Pinf - A @ Pinf @ A, if Pinf and A 
+        are both implemented. Otherwise, uses the Van Loan method to compute
+        Q from the matrix exponential involving the F, L, and Qc
+
+        Overload this method if you have a more general model or simply wish to
+        define the process noise analytically.
+        """
+        try:
+            # See Eq. 7 in Solin & Sarkka 2014
+            # https://users.aalto.fi/~ssarkka/pub/solin_mlsp_2014.pdf
+            Pinf = self.stationary_covariance()
+            A = self.transition_matrix(X1, X2)
+            return Pinf - A @ Pinf @ A.T 
+        except NotImplementedError:
+            # use Van Loan matrix exponential given F, L, Qc
+            dt = X2 - X1
+            F = self.design_matrix()
+            L = self.noise_effect_matrix()
+            Qc = self.noise()
+            return Q_from_VanLoan(F,L,Qc,dt)
+
+
+
+
 
     
 
