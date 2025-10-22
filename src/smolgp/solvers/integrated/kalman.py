de52d46ce30381694cb9a95e5b096e2f6063eda1
Ryan Rubenzahl <rrubenzahl@gmail.com>
2025-10-22 16:57:20 -0400

    copying over integrated functions

--- src/smolgp/solvers/integrated/kalman.py
+++ src/smolgp/solvers/integrated/kalman.py
@@ -6,100 +6,124 @@ import jax.numpy as jnp
 __all__ = ["IntegratedKalmanFilter", "integrated_kalman_filter"]
 
 
-def IntegratedKalmanFilter(kernel, X, y, noise, return_v_S=False):
+def IntegratedKalmanFilter(kernel, X, y, noise, obsid, stateid, return_v_S=False):
     '''
-    Wrapper for Kalman filter
+    Wrapper for jitted integrated_kalman_filter function
 
     Parameters:
-        kernel: StateSpaceModel kernel
-        X: data coordinates (e.g. time)
-        y: observations at data coordinates
+        kernel: IntegratedStateSpaceModel kernel
+        X: Data coordinates with the following structure: (t, texp, instid), where
+            t      : time coordinate
+            texp   : exposure time (same unit as t)
+            instid : instrument id (integer)
+        y: Measurements at the data coordinates
         noise: Noise model
-        
+        obsid: Array of size N, which observation (0,...,N-1) is being made at each state k
+        instids: Array of size N, which instrument (0,...,Ninst-1) recorded observation n
+        stateid: Array of size K, 0 for exposure-start, 1 for exposure-end
+
     Returns:
         m_filtered: filtered means
         P_filtered: filtered covariances
         m_predicted: predicted means
         P_predicted: predicted covariances
     '''
-    # H = kernel.observation_model
-    # A = kernel.transition_matrix
-    # Q = kernel.process_noise
-    # R = noise.diagonal() if noise is not None else jnp.zeros_like(y)
-    # m0 = jnp.zeros(kernel.dimension)
-    # P0 = kernel.stationary_covariance()
 
-    # output = integrated_kalman_filter(A, Q, H, R, X, y, m0, P0)
-    # if return_v_S:
-    #     return output
-    # else:
-    #     m_filtered, P_filtered, m_predicted, P_predicted, v, S = output
-    #     return m_filtered, P_filtered, m_predicted, P_predicted
+    # Unpack data coordinates
+    t, texp, instid = X
+
+    # Model components
+    H_aug = kernel.observation_model
+    A_aug = kernel.transition_matrix
+    Q_aug = kernel.process_noise
+    RESET = kernel.reset_matrix
+    R = noise.diagonal() if noise is not None else jnp.zeros_like(y)
+    
+    # Initial state and covariance
+    mean = jnp.zeros(kernel.d) # TODO: mean function of base kernel
+    m0 = jnp.block([mean] + kernel.num_inst*[jnp.zeros(kernel.d)])
+    P0 = kernel.stationary_covariance()
+
+    output = integrated_kalman_filter(A_aug, Q_aug, H_aug,
+                                      R, t, y, texp, obsid, instid, stateid,
+                                      m0, P0)
+    if return_v_S:
+        return output
+    else:
+        m_filtered, P_filtered, m_predicted, P_predicted, v, S = output
+        return m_filtered, P_filtered, m_predicted, P_predicted
 
 @jax.jit
-def integrated_kalman_filter(A, Q, H, R, t, y, m0, P0):
-    '''
-    Jax implementation of the Kalman filter algorithm
+def integrated_kalman_filter(A_aug, Q_aug, H_aug, RESET,
+                             R, t, y, texp, obsid, instid, stateid, 
+                             m0, P0):
+    """
+    Jax implementation of the integrated Kalman filter algorithm
 
-    See Theorem 4.2 (pdf page 77) in "Bayesian Filtering and Smoothing" 
-    by Simo S{\"a}rkk{\"a} for detailed description of the algorithm and notation.
-
-    e.g. _prev is _{k-1} in Sarkka notation
-         _pred is _k^{-} in Sarkka notation
-
-    Total runtime complexity is O(N*d^3) where N is the number 
-    of time steps and d is the dimension of the state vector.
-    # '''
-    # N = len(t) # number of data points
-
-    # def step(carry, k):
-    #     '''
-    #     Routine for a single step of the Kalman filter
-
-    #     Parameters:
-    #         carry: (x_prev, P_prev) - previous state and covariance
-    #         k: index of the current time step
-        
-    #     Returns:
-    #     - Conditioned state (m_k) and covariance (P_k) to carry to next iteration
-    #     - Full output for completed scan (m_k, P_k, m_pred, P_pred)
-    #     '''
-
-    #     # Unpack previous state and covariance
-    #     m_prev, P_prev = carry
-
-    #     # Logic to check if first time step:
-    #     # If k==0 we use the prior x0, P0
-    #     # and zero time-lag (Delta=0)
-    #     Delta = jax.lax.cond(k > 0,
-    #                          lambda i: t[i] - t[i-1],
-    #                          lambda _: 0.0,
-    #                          k)
-        
-    #     # Get transition matrix
-    #     A_prev = A(0,Delta)
-    #     Q_prev = Q(0,Delta)
-        
-    #     # Predict (Eq. 4.20)
-    #     m_pred = A_prev @ m_prev
-    #     P_pred = A_prev @ P_prev @ A_prev.T + Q_prev 
-        
-    #     # Update (Eq. 4.21)
-    #     H_k = H(t[k])                     # observation model for this time step
-    #     y_pred = H_k @ m_pred             # predicted observation
-    #     v_k = y[k] - y_pred               # "innovation" or "surprise" term
-    #     S_k = H_k @ P_pred @ H_k.T + R[k] # uncertainy in predicted observation
-    #     # S_k_inv = jnp.linalg.inv(S_k)
-    #     # K_k = P_pred @ H_k.T @ S_k_inv    # Kalman gain
-    #     K_k = jnp.linalg.solve(S_k.T, (P_pred @ H_k.T).T).T # more stable
-    #     m_k = m_pred + K_k @ v_k          # conditioned state estimate
-    #     P_k = P_pred - K_k @ S_k @ K_k.T  # conditioned covariance estimate
-        
-    #     return (m_k, P_k), (m_k, P_k, m_pred, P_pred, v_k, S_k)
+    See Section 3.2.1 in Rubenzahl & Hattori et al. (in prep) 
+    for detailed description of the algorithm and notation.
+    """
     
-    # # Initialize carry with prior state and covariance
-    # init_carry = (m0, P0)
+    K = len(t)    # number of iterations (2N)
+    N = int(K/2)
 
-    # # Run the filter over all time steps, unpack, and return results
-    # _, outputs = jax.lax.scan(step, init_carry, jnp.arange(N))
-    # return outputs
+    if len(H) != N:
+        H = jnp.full((N, *H.shape), H)  # case of constant observation model
+
+    def step(carry, k):
+        '''
+        '''
+
+        # Unpack previous state and covariance
+        m_prev, P_prev = carry
+
+        # If k==0 we use the prior m0, Pinf and zero time-lag (dt=0)
+        Delta = jax.lax.cond(k > 0,
+                          lambda i: t[i] - t[i-1],
+                          lambda _: 0.0,
+                          k)
+        n = obsid[k]        # which observation are we working on
+        instid = instid[n] # which instrument this observation is from
+        
+        # Get transition matrix
+        A_prev = A_aug(Delta)
+        Q_prev = Q_aug(Delta)
+
+        # Predict step is same for exposure or gap
+        m_pred = A_prev @ m_prev
+        P_pred = A_prev @ P_prev @ A_prev.T + Q_prev 
+
+        # Update the end of the exposure
+        def update_end():
+
+            y_pred = H_aug @ m_pred             # predicted observation
+            v_k = y[n] - y_pred               # "innovation" or "surprise" term
+            S_k = H_aug @ P_pred @ H_aug.T + R[n] # uncertainy in predicted observation
+            K_k = jnp.linalg.solve(S_k.T, (P_pred @ H_aug.T).T).T # Kalman gain
+            m_k = m_pred + K_k @ v_k          # conditioned state estimate
+            P_k = P_pred - K_k @ S_k @ K_k.T  # conditioned covariance estimate
+            return m_k, P_k, m_pred, P_pred
+
+        # Update the start of the exposure, aka reset its z to zero
+        def update_start():
+            Reset = RESET(instid)
+            m_k = Reset @ m_pred 
+            P_k = Reset @ P_pred @ Reset.T
+            return m_k, P_k, m_pred, P_pred
+        
+        m_k, P_k, m_pred, P_pred = jax.lax.cond(
+                        stateid[k]==0,
+                        lambda _: update_start(), # k=2,4,... is a t_e->t_s aka gap
+                        lambda _: update_end(),   # k=1,3,... is a t_s->t_e aka exposure
+                        operand=None              # note k=0 is -inf->t_s is also a 'gap' update
+                    )  
+        
+        return (m_k, P_k), (m_k, P_k, m_pred, P_pred)
+    
+    # Initialize carry with prior state and covariance
+    init_carry = (m0, P0)
+
+    # Run the filter over all time steps, unpack, and return results
+    _, outputs = jax.lax.scan(step, init_carry, jnp.arange(K))
+    m_filtered, P_filtered, m_predicted, P_predicted = outputs
+    return m_filtered, P_filtered, m_predicted, P_predicted
