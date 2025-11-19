1c1481c7c8bd28fb61311cb138866b6e40ac3d54
Ryan Rubenzahl <rrubenzahl@flatironinstitute.org>
2025-11-18 22:51:48 -0500

    gpu memory benchmarking with nvidia-smi

--- tests/benchmark/benchmark.py
+++ tests/benchmark/benchmark.py
@@ -1,16 +1,13 @@
 from abc import abstractmethod
 import time
 import numpy as np
-
-# import matplotlib as mpl
-import matplotlib.pyplot as plt
-
 import pickle
 import os
-import psutil
-import multiprocessing as mp
-import threading, subprocess
 
+import re
+import psutil
+import threading, subprocess
+import multiprocessing as mp
 mp.set_start_method("spawn", force=True)
 
 import jax
@@ -58,7 +55,7 @@ def load_benchmark_data(filename, unpack=False):
         return data
 
 class MemorySampler:
-    def __init__(self, interval=0.005):
+    def __init__(self, interval=1e-3):
         self.interval = interval
         self.running = False
         self.peak = 0
@@ -95,17 +92,70 @@ class CPUMemorySampler(MemorySampler):
     def fetch_memory(self):
         return self.proc.memory_info().rss
 
+def get_gpu_processes():
+    """
+    Parse the 'Processes' section of NVIDIA SMI
+    Returns dict {pid: used_bytes}.
+    """
+    
+    pid_re = re.compile(r'Process ID\s*:\s*(\d+)')
+    mem_re = re.compile(r'Used GPU Memory\s*:\s*([\d\.]+)\s*MiB', re.IGNORECASE)
+    name_re = re.compile(r'Name\s*:\s*(.*)')
+    type_re = re.compile(r'Type\s*:\s*(\S)')
+
+    # Get the "Processes" section of nvidia-smi
+    cmd = "nvidia-smi --query"
+    out = subprocess.check_output(cmd, shell=True).decode()
+    lines = out.split("\n")
+    for i, line in enumerate(lines):
+        if line.lstrip().startswith("Processes"):
+            break
+        
+    results = {}
+    for line in lines[i:]:
+        line = line.strip()
+
+        # Process ID
+        m_pid = pid_re.search(line)
+        if m_pid:
+            # Start a new record
+            pid = int(m_pid.group(1))
+            results[pid] = {}
+            continue
+
+        # Memory
+        m_mem = mem_re.search(line)
+        if m_mem:
+            mem_mib = float(m_mem.group(1))
+            mem = int(mem_mib * 1024 * 1024)
+            results[pid]['used_memory'] = mem
+            continue
+
+        for reg in [name_re, type_re]:
+            m = reg.search(line)
+            if m:
+                name = reg.pattern.split('\\')[0].lower()
+                results[pid][name] = m.group(1)
+                continue
+
+    return results
+
+
 class GPUMemorySampler(MemorySampler):
     def fetch_memory(self):
-        return int(subprocess.check_output(
-                "nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits",
-                shell=True
-            ).decode().strip())
+        smi = get_gpu_processes()
+        tot_mem = 0
+        for pid in smi:
+            if 'smolgp' in smi[pid]['name']:
+                tot_mem += smi[pid]['used_memory']
+        return tot_mem
 
+    def record_baseline(self, interval=0.1):
+        return 0 
 
-def memthread(fn_bytes, dat_bytes, obj_bytes, args_bytes, return_pipe, machine):
+def tracer(fn_bytes, dat_bytes, obj_bytes, args_bytes, return_pipe, machine):
     """
-    Helper thread for memory profiling.
+    Time and trace memory of a JAX function inside an isolated subprocess.
     """
     # Unpickle function and arguments inside isolated subprocess
     fn = pickle.loads(fn_bytes)
@@ -124,7 +174,7 @@ def memthread(fn_bytes, dat_bytes, obj_bytes, args_bytes, return_pipe, machine):
         out.block_until_ready()
 
     if machine == "gpu":
-        sampler = GPUMemorySampler(interval=1e-5)
+        sampler = GPUMemorySampler(interval=1e-2)
     elif machine == "cpu":
         sampler = CPUMemorySampler(interval=1e-5)
     else:
@@ -139,6 +189,7 @@ def memthread(fn_bytes, dat_bytes, obj_bytes, args_bytes, return_pipe, machine):
     if hasattr(out, "block_until_ready"):
         out.block_until_ready()
     end = time.perf_counter()
+    time.sleep(0.1)
     sampler.stop()
 
     # Return both result (pickled) and stats
@@ -167,7 +218,7 @@ def profile_jax_function(fn, data, obj, *args, n_repeat=5, machine="cpu", **kwar
     for _ in range(n_repeat):
         parent_conn, child_conn = mp.Pipe()
         p = mp.Process(
-            target=memthread, args=(fn_bytes, dat_bytes, obj_bytes, args_bytes, child_conn, machine)
+            target=tracer, args=(fn_bytes, dat_bytes, obj_bytes, args_bytes, child_conn, machine)
         )
         p.start()
         result = parent_conn.recv()
@@ -185,7 +236,7 @@ def profile_jax_function(fn, data, obj, *args, n_repeat=5, machine="cpu", **kwar
 
 def benchmark(funcs, data, objs, *args, n_repeat=3, cutoffs={}, use_gpu_profiler=False):
     """
-    Given some (jitted) functions, benchmark their runtimes over a range of input sizes.
+    Given some (to-be-jitted) functions, benchmark their runtimes over a range of input sizes.
 
     Parameters
     ----------
@@ -209,6 +260,7 @@ def benchmark(funcs, data, objs, *args, n_repeat=3, cutoffs={}, use_gpu_profiler
     memory = {}
     outputs = {}
     Ns = []
+    machine = "gpu" if use_gpu_profiler else "cpu"
     for n in range(len(data)):
         N = data[n].shape[-1]
         Ns.append(N)
@@ -219,14 +271,9 @@ def benchmark(funcs, data, objs, *args, n_repeat=3, cutoffs={}, use_gpu_profiler
             cutoff = cutoffs.get(name, 3e4)
 
             if N <= cutoff:
-                if use_gpu_profiler:
-                    t, mem, val = profile_jax_function(
-                        func, data[n], obj, *args, n_repeat=n_repeat, machine="gpu"
-                    )
-                else:
-                    t, mem, val = profile_jax_function(
-                        func, data[n], obj, *args, n_repeat=n_repeat, machine="cpu"
-                    )
+                t, mem, val = profile_jax_function(
+                    func, data[n], obj, *args, n_repeat=n_repeat, machine=machine
+                )
                 basestr = f"    {name}: time = {t[0]:.4f} ± {t[1]:.4f} s"
                 memstr = f", mem = {format_bytes(mem[0])} ± {format_bytes(mem[1])}"
                 print(basestr + memstr)
@@ -243,7 +290,6 @@ def benchmark(funcs, data, objs, *args, n_repeat=3, cutoffs={}, use_gpu_profiler
             memory[name].append(mem)
             outputs[name].append(val)
 
-            # TODO: save per-iteration here and combine into table, skip if already done
             save_benchmark_data(f"results/individual/{func.__name__}_{N}.pkl", 
                                 [N], {name: [t]}, {name: [mem]}, {name: [val]})
 
