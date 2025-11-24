4c6ad050a5ad83476a5275b1dff63bd3f4283301
Soichiro Hattori <soichiro.hattori@gmail.com>
2025-11-24 14:03:13 -0500

    enable autoapi for docs

--- docs/conf.py
+++ docs/conf.py
@@ -25,3 +25,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
+
+# AutoAPI
+extensions = ["autoapi.extension"]
+autoapi_dirs = ["../src"]
