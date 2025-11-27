ca41caa1dd50f8254c1f438ea91d2a51d4b32b16
Ryan Rubenzahl <rrubenzahl@gmail.com>
2025-11-26 19:26:58 -0500

    'Proofs' for analytic and augmented matrices

---
+++ tests/test_proofs.py
@@ -0,0 +1,137 @@
+import jax
+import jax.numpy as jnp
+import smolgp
+from jax.scipy.linalg import expm
+
+key = jax.random.PRNGKey(0)
+jax.config.update("jax_enable_x64", True)
+
+
+def allclose(name, residuals, tol, atol=1e-14):
+    """
+    Check all residuals are < tol
+    if they are, but aren't < atol, print a warning
+    """
+    maxres = jnp.max(jnp.abs(residuals))
+    assert maxres < tol, (
+        f"{name} did not agree to within desired tolerance."
+        f" Maximum absolute deviation is {maxres:.3e} "
+    )
+    if maxres < atol:
+        print(f"    ...{name}: agrees exactly (<{maxres:.0e})")
+    else:
+        print(f"    ...{name}: agrees (WARNING: only to < {maxres:.1e})")
+
+
+def test_base(kernel, dts):
+    """
+    Confirm all analytic versions of matrices
+    agree with their numerical versions--
+    we're assuming `kernel` has analytic A and Q defined
+    """
+
+    # For (underdamped) SHO, these are analytic functions
+    transition_matrix_analytic = lambda dt: kernel.transition_matrix(0, dt)
+    process_noise_analytic = lambda dt: kernel.process_noise(0, dt)
+
+    # Numerical versions
+    # transition_matrix_numerical = lambda dt: expm(F * dt)
+    transition_matrix_numerical = lambda dt: super(
+        type(kernel), kernel
+    ).transition_matrix(0, dt)
+    process_noise_vanloan = lambda dt: super(type(kernel), kernel).process_noise(
+        0, dt, use_van_loan=True
+    )
+    process_noise_fromPinf = lambda dt: super(type(kernel), kernel).process_noise(
+        0, dt, use_van_loan=False
+    )
+
+    # Test all dts
+    print("Comparing analytic Phi vs. expm(F*dt)...")
+    A_analytic = jax.vmap(transition_matrix_analytic)(dts)
+    A_numerical = jax.vmap(transition_matrix_numerical)(dts)
+    allclose(
+        "Transition matrix",
+        A_analytic - A_numerical,
+        tol=1e-9,
+        atol=1e-12,
+    )
+    print("Comparing analytic Q vs. VanLoan Q...")
+    Q_analytic = jax.vmap(process_noise_analytic)(dts)
+    Q_vanloan = jax.vmap(process_noise_vanloan)(dts)
+    allclose(
+        "Process noise (VanLoan)",
+        Q_analytic - Q_vanloan,
+        tol=1e-9,
+        atol=1e-12,
+    )
+    print("Comparing analytic Q vs. Q from Pinf...")
+    Q_fromPinf = jax.vmap(process_noise_fromPinf)(dts)
+    allclose(
+        "Process noise (from Pinf)",
+        Q_analytic - Q_fromPinf,
+        tol=1e-9,
+        atol=1e-12,
+    )
+    print("All base matrix tests passed.")
+
+
+def test_integrated(kernel, dts):
+    """
+    Confirm all analytic versions of matrices and their
+    augmented forms agree with their numerical versions--
+    we're assuming `kernel` has analytic Phibar defined
+    """
+
+    ### Integrated transition matrix
+    Phibar_analytic = lambda dt: kernel.integrated_transition_matrix(0, dt)
+    Phibar_from_VanLoan = lambda dt: super(
+        type(kernel), kernel
+    ).integrated_transition_matrix(0, dt)
+    print("Comparing analytic Phibar vs. Van Loan...")
+    A_analytic = jax.vmap(Phibar_analytic)(dts)
+    A_numerical = jax.vmap(Phibar_from_VanLoan)(dts)
+    allclose(
+        "Integrated transition matrix",
+        A_analytic - A_numerical,
+        tol=1e-8,
+        atol=1e-12,
+    )
+
+    ### Augmented transition matrix
+    F_aug = kernel.design_matrix()
+    Phiaug_numerical = lambda dt: expm(F_aug * dt)
+    Phiaug_analytic = lambda dt: kernel.transition_matrix(0, dt)
+    print("Comparing analytic Phiaug vs. expm(Faug*dt)...")
+    Aaug_analytic = jax.vmap(Phiaug_analytic)(dts)
+    Aaug_numerical = jax.vmap(Phiaug_numerical)(dts)
+    allclose(
+        "Augmented transition matrix",
+        Aaug_analytic - Aaug_numerical,
+        tol=1e-5,
+        atol=1e-12,
+    )
+    print("All integrated/augmented matrix tests passed.")
+
+
+if __name__ == "__main__":
+    ## TODO: do we also want to test shapes here?
+
+    ## This test is done with the SHO Kernel, since
+    ## we have analytic versions to compare against
+    S = 2.36
+    w = 0.0195
+    Q = 7.63
+    sigma = jnp.sqrt(S * w * Q)
+    sho = smolgp.kernels.SHO(omega=w, quality=Q, sigma=sigma)
+    isho = smolgp.kernels.IntegratedSHO(omega=w, quality=Q, sigma=sigma)
+
+    ## Test a wide dynamic range of Deltas
+    ## NOTE: expm breaks down around ~1e6
+    dts = jnp.append(jnp.array([0.0]), jnp.logspace(-6, 5, 1000))
+
+    print("Testing base (instantaneous) kernel matrices...")
+    test_base(sho, dts)
+    print()
+    print("Testing integrated kernel matrices...")
+    test_integrated(isho, dts)
