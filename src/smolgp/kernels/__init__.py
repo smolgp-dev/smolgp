b0b0035a2d74a49ae50904511dd51826dd65c5c9
Ryan Rubenzahl <rrubenzahl@gmail.com>
2026-01-14 13:01:41 -0500

    update docstring

--- src/smolgp/kernels/__init__.py
+++ src/smolgp/kernels/__init__.py
@@ -4,10 +4,16 @@ typically constructed as sums and products of objects defined in this
 subpackage, or by subclassing :class:`Kernel` as discussed in the :ref:`kernels`
 tutorial. The kernels implemented here are extensions of those defined in
 ``tinygp.kernels.quasisep`` to be fully-compatible with the state space solvers.
+The key differences are that `smolgp` kernels:
+1. Include the `noise_effect_matrix` and `process_noise` matrix
+2. Treat the observation model as a row vector that can be selected on/off via the kernel's name
+3. The transition matrix is in its usual form (compared to transposed forms in tinygp.kernels.quasisep)
 
 For modeling integrated measurements, use the kernels in ``smolgp.kernels.integrated``.
 
 Sums and Products of ``smolgp`` kernels are also ``smolgp`` kernels.
+Their conditioned and predictive distributions can be decomposed per-component
+using GaussianProcess.component_means and predict_component_means, respectively.
 
 For mixed kernels containing some integrated and some instantaneous components,
 the integrated solver will be used to handle the entire kernel.
