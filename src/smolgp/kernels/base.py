Fix PSD normalization
Removed division by 2pi in compute_psd function, which brings this calculation into agreement with the definition in Solin & Sarkka (2014) as well as Rubenzahl et al. (2026) in which the PSD S(omega) is defined in terms of the angular frequency (omega in rad/sec)

src/smolgp/kernels/base.py
@@ -241,7 +241,7 @@ class StateSpaceModel(Kernel):
         def compute_psd(w: JAXArray) -> JAXArray:
             M = jnp.linalg.inv(F + 1j * w * I)
             S = H @ M.conj() @ L @ Qc @ L.T @ M.T @ H.T
-            return S.squeeze().real / (2 * jnp.pi)
+            return S.squeeze().real
 
         return jax.vmap(compute_psd)(omega)
 